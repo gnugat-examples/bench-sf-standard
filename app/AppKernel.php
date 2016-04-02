5fd01e87e0c59bccc054a7900aa594f8a36656a3
Loïc Faugeron <faugeron.loic@gmail.com>
2016-04-02 10:38:49 +0100

    Benchmarked M6Web bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,6 +15,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new M6Web\Bundle\PhpProcessManagerBundle\M6WebPhpProcessManagerBundle(),
             new AppBundle\AppBundle(),
         ];
 
